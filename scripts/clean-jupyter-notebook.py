40b5eddb4d7cd78774e31022a86505ddaeaf2e42
Alba Torres <albayurani.torres@gmail.com>
2025-03-02 15:49:57 -0800

    Automatic jupyter notebook cleanup

---
+++ scripts/clean-jupyter-notebook.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# Remove generated output from Jupyter files to get clear and meaningful diffs
+# This runs automatically when doing a "git commit" to ensure code quality
+
+import sys
+import json
+from nbformat import read, write, NO_CONVERT
+
+def strip_output(nb):
+    for cell in nb.cells:
+        if cell.cell_type == 'code':
+            cell.outputs = []
+            cell.execution_count = None
+    return nb
+
+if __name__ == "__main__":
+    nb = read(sys.stdin, as_version=NO_CONVERT)
+    nb = strip_output(nb)
+    write(nb, sys.stdout)
+    sys.stdout.write("\n")
